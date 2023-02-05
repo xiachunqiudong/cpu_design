3a515eecc873630d5f22dd9ae8a3e748cba55d2b
xiachunqiudong <xiadaoliang@outlook.com>
2023-02-05 16:13:47 +0800

    异常处理

--- pipe/rtl/obj_dir/cpu.d
+++
@@ -1,7 +0,0 @@
-cpu.o: ../cpu.cpp /usr/local/share/verilator/include/verilated_vcd_c.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_trace.h \
- /usr/local/share/verilator/include/verilated_trace_defs.h Vcpu.h \
- /usr/local/share/verilator/include/verilated_heavy.h \
- /usr/local/share/verilator/include/verilated.h
--- pipe/rtl/obj_dir/top_sim.d
+++
@@ -1,8 +0,0 @@
-top_sim.o: ../top_sim.cpp \
- /usr/local/share/verilator/include/verilated_vcd_c.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_trace.h \
- /usr/local/share/verilator/include/verilated_trace_defs.h Vtop_sim.h \
- /usr/local/share/verilator/include/verilated_heavy.h \
- /usr/local/share/verilator/include/verilated.h
